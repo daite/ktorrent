e438bce4df6b52f78eecc49202626b3376aad2c7
daite <blueskykind02@yahoo.co.jp>
2021-02-09 13:43:11 +0900

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,54 @@
-use select::document::Document;
-use select::predicate::{Class, Name};
+pub use select::document::Document;
+pub use select::predicate::{Class, Name};
+pub use scrape::*;
 
+/// # Example
+/// ```
+/// let bbs_doc = ktorrent::Document::from(include_str!("./test_data/torrentsir_bbs.html"));
+/// let data = ktorrent::find_child_attr_by_tag(
+///    &bbs_doc, "list-group", "a", "href")[1];
+///  assert_eq!(
+///     "magnet:?xt=urn:btih:27646d3df274ed51b6386bd6aa40da849a73b341",
+///      data,
+///  );
+/// ```
 pub mod scrape {
     use super::*;
-    pub fn get_data_by_class_name<'a> (
+    /// it collects a child attribute by a child tag name.
+	/// <li class="p_class_name">
+    ///     <i class="c_class_name"></i> 
+    ///     <c_tag_name c_attr_name=c_attr_name>xxx</a>
+    /// </li>
+    pub fn find_child_attr_by_tag<'a> (
         doc: &'a Document, 
-        class_name: &'a str, 
-        tag_name: &'a str,
-        attr_name: &'a str) -> Vec<&'a str> {
+        p_class_name: &'a str, 
+        c_tag_name: &'a str,
+        c_attr_name: &'a str) -> Vec<&'a str> {
         let mut data = vec![];
-        for node in doc.find(Class(class_name)) {
-            let val = node.find(Name(tag_name))
+        for node in doc.find(Class(p_class_name)) {
+            let val = node.find(Name(c_tag_name))
                         .next()
                         .unwrap()
-                        .attr(attr_name)
+                        .attr(c_attr_name)
                         .unwrap();
             data.push(val);     
         }
         data
     }
-    pub fn get_data_by_tag_name(
+    /// it collects a text from a parent node which has a class name of a child.
+    /// <b>
+    /// <b class="c_class_name">child_text</b>
+    /// parent_text
+    /// </b>
+    pub fn find_parent_text(
         doc: &Document, 
-        tag_name: &str, 
-        class_name: &str) -> Vec<String> {
+        p_tag_name: &str, 
+        c_class_name: &str) -> Vec<String> {
         let mut data = vec![];
-        for node in doc.find(Name(tag_name)) {
+        for node in doc.find(Name(p_tag_name)) {
             for n in node.children() {
                 for (_, cls_name) in n.attrs() {
-                    if cls_name == class_name {
+                    if cls_name == c_class_name {
                         data.push(node.text());
                     } 
                 }
@@ -35,7 +56,11 @@ pub mod scrape {
         }
         data
     }
-    pub fn get_data_by_only_class_name<'a>(
+    /// it collects all texts by a class name.
+    /// <a href="XX" class=class_name title="XXXX">text</a>
+    /// parent_text
+    /// </b>
+    pub fn find_all_text_by_class<'a>(
         doc: &'a Document, 
         class_name: &'a str) -> Vec<String> {     
         let mut data = vec![];
@@ -44,17 +69,26 @@ pub mod scrape {
         }
         data
     }
-    pub fn get_data_by_tag_name_with(
+    /// it collects a child attribute by a child class name.
+    ///<td>
+    ///<a class="class_name" href="XXXX" onclick=XXXX>
+    ///<span class="fa fa-cloud-download"></span>
+    ///</a>
+    ///<a class="class_name" href="XXXX onclick="XXXX rel="XXXX">
+    ///<span class="fa fa-magnet"></span>
+    //</a>
+    //</td>
+    pub fn find_child_attr_by_class(
         doc: &Document, 
-        tag_name: &str,
-        class_name: &str, 
-        attr_name: &str) -> Vec<String> {
+        p_tag_name: &str,
+        c_class_name: &str, 
+        c_attr_name: &str) -> Vec<String> {
         let mut data = vec![];
-        for node in doc.find(Name(tag_name)) {
+        for node in doc.find(Name(p_tag_name)) {
             for n in node.children() {
                 for (_, cls_name) in n.attrs() {
-                    if cls_name == class_name {
-                        data.push(n.attr(attr_name).unwrap().to_owned());
+                    if cls_name == c_class_name {
+                        data.push(n.attr(c_attr_name).unwrap().to_owned());
                     } 
                 }
             }
@@ -69,7 +103,7 @@ mod tests {
     #[test]
     fn test_get_magnet_for_torrentsir() {
         let bbs_doc = Document::from(include_str!("./test_data/torrentsir_bbs.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &bbs_doc, "list-group", "a", "href")[1];
         assert_eq!(
             "magnet:?xt=urn:btih:27646d3df274ed51b6386bd6aa40da849a73b341",
@@ -79,7 +113,7 @@ mod tests {
     #[test]
     fn test_get_get_title_for_torrentsir() {
         let search_doc = Document::from(include_str!("./test_data/torrentsir_search.html"));
-        let data = scrape::get_data_by_tag_name(&search_doc, "b", "sch_word");
+        let data = scrape::find_parent_text(&search_doc, "b", "sch_word");
         assert_eq!(
             "동상이몽2 너는 내운명.E182.210201.720p-NEXT",
             &data[0],
@@ -88,7 +122,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_torrentsir() {
         let search_doc = Document::from(include_str!("./test_data/torrentsir_search.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &search_doc, "media-heading", "a", "href")[0];
         assert_eq!(
             "./board.php?bo_table=entertain&wr_id=18170",
@@ -98,7 +132,7 @@ mod tests {
     #[test]
     fn test_get_magnet_for_torrentj() {
         let bbs_doc = Document::from(include_str!("./test_data/torrentj_bbs.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &bbs_doc, "list-group", "a", "href")[1];
         assert_eq!(
             "magnet:?xt=urn:btih:27646d3df274ed51b6386bd6aa40da849a73b341",
@@ -108,7 +142,7 @@ mod tests {
     #[test]
     fn test_get_title_name_for_torrentj() {
         let search_doc = Document::from(include_str!("./test_data/torrentj_search.html"));
-        let data = scrape::get_data_by_tag_name(&search_doc, "b", "sch_word");
+        let data = scrape::find_parent_text(&search_doc, "b", "sch_word");
         assert_eq!(
             "동상이몽2 너는 내운명.E182.210201.720p-NEXT",
             &data[0],
@@ -117,7 +151,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_torrentj() {
         let search_doc = Document::from(include_str!("./test_data/torrentj_search.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &search_doc, "media-heading", "a", "href")[0];
         assert_eq!(
             "./board.php?bo_table=entertain&wr_id=18172",
@@ -127,7 +161,7 @@ mod tests {
     #[test]
     fn test_get_title_for_torrentview() {
         let search_doc = Document::from(include_str!("./test_data/torrentview_search.html"));
-        let data = scrape::get_data_by_tag_name(&search_doc, "b", "sch_word");
+        let data = scrape::find_parent_text(&search_doc, "b", "sch_word");
         assert_eq!(
             "동상이몽2 너는 내운명.E182.210201.720p-NEXT",
             &data[0],
@@ -136,7 +170,7 @@ mod tests {
     #[test]
     fn test_get_magnet_for_torrentview() {
         let bbs_doc = Document::from(include_str!("./test_data/torrentview_bbs.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &bbs_doc, "list-group", "a", "href")[1];
         assert_eq!(
             "magnet:?xt=urn:btih:27646d3df274ed51b6386bd6aa40da849a73b341",
@@ -146,7 +180,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_torrentview() {
         let search_doc = Document::from(include_str!("./test_data/torrentview_search.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &search_doc, "media-heading", "a", "href")[0];
         assert_eq!(
             "./board.php?bo_table=enter&wr_id=21971",
@@ -156,7 +190,7 @@ mod tests {
     #[test]
     fn test_get_title_name_for_tshare() {
         let search_doc = Document::from(include_str!("./test_data/tshare_search.html"));
-        let data = scrape::get_data_by_tag_name(&search_doc, "p", "sch_word");
+        let data = scrape::find_parent_text(&search_doc, "p", "sch_word");
         assert_eq!(
             " 아는사이.2020.720p.HDRip-mov18  아는사이.2020.720p.HDRip-mov18",
             &data[0],
@@ -165,7 +199,7 @@ mod tests {
     #[test]
     fn test_get_magnet_for_tshare() {
         let bbs_doc = Document::from(include_str!("./test_data/tshare_bbs.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &bbs_doc, "board-view-torrent-info", "a", "href")[0];
         assert_eq!(
             "magnet:?xt=urn:btih:77c904927c0067cb3aadedae461e20c08eb11164",
@@ -175,7 +209,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_tshare() {
         let search_doc = Document::from(include_str!("./test_data/tshare_search.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &search_doc, "list-item-row", "a", "href")[0];
         assert_eq!(
             "https://tshare.org/movie/11565",
@@ -185,7 +219,7 @@ mod tests {
     #[test]
     fn test_get_title_for_torrentmobile() {
         let search_doc = Document::from(include_str!("./test_data/torrentmobile_search.html"));
-        let data = scrape::get_data_by_tag_name(&search_doc, "b", "sch_word");
+        let data = scrape::find_parent_text(&search_doc, "b", "sch_word");
         assert_eq!(
             "동상이몽2 너는 내운명.E182.210201.720p-NEXT",
             &data[0],
@@ -194,7 +228,7 @@ mod tests {
     #[test]
     fn test_get_magnet_torrentmobile() {
         let bbs_doc = Document::from(include_str!("./test_data/torrentmobile_bbs.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &bbs_doc, "list-group", "a", "href")[0];
         assert_eq!(
             "magnet:?xt=urn:btih:27646d3df274ed51b6386bd6aa40da849a73b341",
@@ -204,7 +238,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_torrentmobile() {
         let search_doc = Document::from(include_str!("./test_data/torrentmobile_search.html"));
-        let data = scrape::get_data_by_class_name(
+        let data = scrape::find_child_attr_by_tag(
             &search_doc, "media-heading", "a", "href")[0];
         assert_eq!(
             "./board.php?bo_table=music&wr_id=50564",
@@ -214,7 +248,7 @@ mod tests {
     #[test]
     fn test_get_title_for_ttobogo() {
         let search_doc = Document::from(include_str!("./test_data/ttobogo_search.html"));
-        let data = &scrape::get_data_by_only_class_name(&search_doc, "subject")[0];
+        let data = &scrape::find_all_text_by_class(&search_doc, "subject")[0];
         assert_eq!(
             "동상이몽2 너는 내운명.E182.210201.720p-NEXT",
             data,
@@ -223,7 +257,7 @@ mod tests {
     #[test]
     fn test_get_magnet_ttobogo() {
         let bbs_doc = Document::from(include_str!("./test_data/ttobogo_bbs.html"));
-        let data = &scrape::get_data_by_tag_name_with(
+        let data = &scrape::find_child_attr_by_class(
             &bbs_doc, "td", "btn btn-blue", "onclick")[0];
         assert_eq!(
             "file_download(\'magnet:?xt=urn:btih:27646d3df274ed51b6386bd6aa40da849a73b341\')",
@@ -233,7 +267,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_ttobogo() {
         let search_doc = Document::from(include_str!("./test_data/ttobogo_search.html"));
-        let data = &scrape::get_data_by_tag_name_with(
+        let data = &scrape::find_child_attr_by_class(
             &search_doc, "div", "subject", "href")[0];
         assert_eq!(
             "https://www1.ttobogo.net/post/192852",
@@ -243,7 +277,7 @@ mod tests {
     #[test]
     fn test_get_title_for_torrentsee() {
         let search_doc = Document::from(include_str!("./test_data/torrentsee_search.html"));
-        let data = scrape::get_data_by_only_class_name(&search_doc, "tit");
+        let data = scrape::find_all_text_by_class(&search_doc, "tit");
         assert_eq!(
             "동상이몽2너는내운명.E138.200323.720p-NEXT",
             data[1].trim(),
@@ -252,7 +286,7 @@ mod tests {
     #[test]
     fn test_get_magnet_torrentsee() {
         let bbs_doc = Document::from(include_str!("./test_data/torrentsee_bbs.html"));
-        let data = &scrape::get_data_by_tag_name(
+        let data = &scrape::find_parent_text(
             &bbs_doc, "td", "bbs_btn2")[1];
         assert_eq!(
             "magnet:?xt=urn:btih:eee4d6fdf36ba112523cc48315ac5300cd84c77f",
@@ -262,7 +296,7 @@ mod tests {
     #[test]
     fn test_get_get_bbs_url_for_torrentsee() {
         let search_doc = Document::from(include_str!("./test_data/torrentsee_search.html"));
-        let data = scrape::get_data_by_class_name(&search_doc, "tit", "a", "href")[0];
+        let data = scrape::find_child_attr_by_tag(&search_doc, "tit", "a", "href")[0];
         assert_eq!(
             "/topic/106593",
             data,
